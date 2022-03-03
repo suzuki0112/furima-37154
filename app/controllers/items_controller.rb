cb0e6ba0aec785fca4eba85a33d84d634f8c1166
suzuki0112 <banana.is.justice@gmail.com>
2022-03-04 07:23:23 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,14 @@
 class ItemsController < ApplicationController
-   
   before_action :move_to_index, only: [:new]
 
   def index
+    @items = Item.order('created_at DESC')
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -19,7 +21,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :freight_id, :area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :freight_id, :area_id,
+                                 :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
   def move_to_index
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,19 +74,19 @@ RSpec.describe User, type: :model do
         @user.password = 'aaaaaa'
         @user.password_confirmation = 'aaaaaa'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it 'passwordが数字のみでは登録できない' do
         @user.password = '000000'
         @user.password_confirmation = '000000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it '全角文字を含むパスワードでは登録できない' do
         @user.password = 'ａaa000'
         @user.password_confirmation = 'ａaa000'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it 'emailは@を含まないと登録できない' do
         @user.email = 'testmail'
@@ -96,42 +96,42 @@ RSpec.describe User, type: :model do
       it 'first_nameが半角では登録できない' do
         @user.first_name = 'ﾃｽﾄ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid")
+        expect(@user.errors.full_messages).to include('First name is invalid')
       end
       it 'family_nameが半角では登録できない' do
         @user.family_name = 'ﾃｽﾄ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Family name is invalid")
+        expect(@user.errors.full_messages).to include('Family name is invalid')
       end
       it 'read_firstが半角では登録できない' do
         @user.read_first = 'ﾃｽﾄ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Read first is invalid")
+        expect(@user.errors.full_messages).to include('Read first is invalid')
       end
       it 'read_familyが半角では登録できない' do
         @user.read_family = 'ﾃｽﾄ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Read family is invalid")
+        expect(@user.errors.full_messages).to include('Read family is invalid')
       end
       it 'read_firstが漢字では登録できない' do
         @user.read_first = '漢字'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Read first is invalid")
+        expect(@user.errors.full_messages).to include('Read first is invalid')
       end
       it 'read_fimilyが漢字では登録できない' do
         @user.read_family = '漢字'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Read family is invalid")
+        expect(@user.errors.full_messages).to include('Read family is invalid')
       end
       it 'read_firstがひらがなでは登録できない' do
         @user.read_first = 'ひらがな'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Read first is invalid")
+        expect(@user.errors.full_messages).to include('Read first is invalid')
       end
       it 'read_familyがひらがなでは登録できない' do
         @user.read_family = 'ひらがな'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Read family is invalid")
+        expect(@user.errors.full_messages).to include('Read family is invalid')
       end
     end
   end

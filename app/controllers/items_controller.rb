1d0af9a53e14d09ea7bbfee3071836a0d5dc2b9f
suzuki0112 <banana.is.justice@gmail.com>
2022-03-11 07:49:01 +0900

    商品情報編集機能
    # What
    商品情報編集機能
    # Why
    商品情報編集機能実装のため
    
    ログイン状態の出品者は、商品情報編集ページに遷移できる動画
    https://gyazo.com/9ad558c5625ad9983742455d58fdfee9
    
    必要な情報を適切に入力して「更新する」ボタンを押すと、商品の情報を編集できる動画
    https://gyazo.com/333dd63d667ee6bdeb40edb2923eaeaf
    
    入力に問題がある状態で「変更する」ボタンが押された場合、情報は保存されず、編集ページに戻りエラーメッセージが表示される動画
    https://gyazo.com/ae074ee506a3a70075c59183d68c955a
    
    何も編集せずに「更新する」ボタンを押しても、画像無しの商品にならない動画
    https://gyazo.com/ba6b0483c930698b711e04910588ab10
    
    ログイン状態の場合でも、URLを直接入力して自身が出品していない商品の商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずトップページに遷移する動画
    https://gyazo.com/1c91a8311b0a2eba215a7959efbf9515
    
    ログアウト状態の場合は、URLを直接入力して商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずログインページに遷移する動画
    https://gyazo.com/87e1fab2fe1a587dc4bc4c6e47ade333
    
    商品名やカテゴリーの情報など、すでに登録されている商品情報は商品情報編集画面を開いた時点で表示される動画（商品画像・販売手数料・販売利益に関しては、表示されない状態で良い）
    https://gyazo.com/54b30f2ec56feddbd66c658921d76d42

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, only: [:new]
+  before_action :move_to_index, only: [:new, :edit]
+  before_action :move_to_root_path, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,8 +21,17 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-
-
+  def edit
+    @item = Item.find(params[:id])
+  end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
   private
 
   def item_params
@@ -31,5 +41,12 @@ class ItemsController < ApplicationController
 
   def move_to_index
     authenticate_user!
+  end
+ 
+  def move_to_root_path
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end 
   end
 end

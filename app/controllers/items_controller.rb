d6c28283a159607f322a1cc11b4517e9d4d03724
suzuki0112 <banana.is.justice@gmail.com>
2022-03-14 07:44:54 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new, :edit]
   before_action :move_to_root_path, only: [:edit]
+  before_action :item_find, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,13 +20,10 @@ class ItemsController < ApplicationController
     end
   end
   def show
-    @item = Item.find(params[:id])
   end
   def edit
-    @item = Item.find(params[:id])
   end
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -48,5 +46,8 @@ class ItemsController < ApplicationController
     if current_user.id != @item.user_id
       redirect_to root_path
     end 
+  end
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
